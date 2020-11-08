9907f5d61209fbbc2636c0208fbea695ef721d9f
Kevin Cho <kccho2254@gmail.com>
2020-11-07 21:32:44 -0600

    safety push before restarting computer for windows updates

--- script.js
+++ script.js
@@ -8,18 +8,24 @@
 // If I select the correct option, display "Correct!"
 // else display "Wrong!"
 // AND display: none on that question and move to the next question
-
-
 // Calvin's HW demo instructions
+
+
+// Add scores to your total score!
 
 var add = (function () {
   var counter = 0;
   return function () {counter += 1; return counter;}
+ 
+
 })();
 
 function myFunction(){
   document.getElementById("myScore").innerHTML = "Correct answers: " + add();
-}
+  if ($('#returnBtn').click()) {
+    counter = 0;
+  }
+} 
 
 $(".first-question").hide();
 $('.second-question').hide();
@@ -34,6 +40,7 @@ $('.tenth-question').hide();
 $('.eleventh-question').hide();
 $(".leaderboard").hide();
 
+// Cycling through questions
 
 $(document).ready(function(){
 
@@ -166,4 +173,83 @@ $(document).ready(function(){
 
   });
 
+});
+
+// Return button 
+
+$(document).ready(function(){
+
+  $("#returnBtn").on("click", function(){    
+
+    $(".first-question").hide();
+    $('.second-question').hide();
+    $('.third-question').hide();
+    $('.fourth-question').hide();
+    $('.fifth-question').hide();
+    $('.sixth-question').hide();
+    $('.seventh-question').hide();
+    $('.eighth-question').hide();
+    $('.ninth-question').hide();
+    $('.tenth-question').hide();
+    $('.eleventh-question').hide();
+    $(".leaderboard").hide();
+    $("#startPoint").show();
+
+  });
+});
+
+// Leaderboard stuff goes here
+
+$(document).ready(function(){
+
+  $("#leaderboardInt").on("click", function(){
+    
+    $(".first-question").hide();
+    $('.second-question').hide();
+    $('.third-question').hide();
+    $('.fourth-question').hide();
+    $('.fifth-question').hide();
+    $('.sixth-question').hide();
+    $('.seventh-question').hide();
+    $('.eighth-question').hide();
+    $('.ninth-question').hide();
+    $('.tenth-question').hide();
+    $('.eleventh-question').hide();
+    $("#startPoint").hide();
+    $(".leaderboard").show();
+
+  });
+});
+
+// myInitials and myHiscore to be linked from HTML
+// myAttempt at leaderboard
+// Local Storage shenanigans go here
+
+var initialsSpan = document.querySelector("#myInitials");
+var hiscoreSpan = document.querySelector("#myHiscore");
+var signUpButton = document.querySelector("#eleventh-hide");
+
+
+function renderLastRegistered() {
+  var initials = localStorage.getItem("initials");
+  var hiscore = localStorage.getItem("hiscore");
+
+  if (!initials || !hiscore) {
+    return;
+  }
+
+  initialsSpan.textContent = initials;
+  hiscoreSpan.textContent = hiscore;
+}
+
+signUpButton.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var initials = document.querySelector("#myInitials").value;
+  var hiscore = document.querySelector("#myHiscore").value;
+
+    localStorage.setItem("initial", initials);
+    localStorage.setItem("hiscore", hiscore);
+    renderLastRegistered();
+  
 });
