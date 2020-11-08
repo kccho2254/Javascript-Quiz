96fdb90ef4bfb0c490e4f18883459a31847165b7
Kevin Cho <kccho2254@gmail.com>
2020-11-07 18:17:15 -0600

    created sample questions, added javascript to make the questions function properly, linked jQuery elements/library, formatted css within HTML

---
+++ script.js
@@ -0,0 +1,91 @@
+// var uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh = poggers;
+// // LOOK AT ACTIVITY 18-STU_BOOTSTRAP-EXTEND FOR HOMEWORK AND 06-STU_FORM-ELEMENTS AND 09-INS_EVENT-DELEGATION
+// var formEl = $('#start-form');
+
+
+// when I click the start button, display: none on <main> and display the next question
+// Display the next question and the four options
+// If I select the correct option, display "Correct!"
+// else display "Wrong!"
+// AND display: none on that question and move to the next question
+
+
+// Calvin's HW demo instructions
+
+var add = (function () {
+    var counter = 0;
+    return function () {counter += 1; return counter;}
+  })();
+  
+  function myFunction(){
+    document.getElementById("myScore").innerHTML = "Correct answers: " + add();
+  }
+
+
+$(".first-question").hide();
+$('.second-question').hide();
+$('.third-question').hide();
+$('.fourth-question').hide();
+$('.fifth-question').hide();
+$('.sixth-question').hide();
+$('.seventh-question').hide();
+$('.eighth-question').hide();
+$('.ninth-question').hide();
+$('.tenth-question').hide();
+$('.eleventh-question').hide();
+
+$(document).ready(function(){
+
+    $("#home-hide").click(function(){
+
+      $(".first-question").show();
+      $(".start-page").hide();
+
+    });
+
+  });
+
+$(document).ready(function(){
+
+    $("#first-hide").click(function(){
+
+      $(".first-question").hide();
+      $(".second-question").show();
+
+    });
+
+});
+
+$(document).ready(function(){
+
+    $("#second-hide").click(function(){
+
+      $(".second-question").hide();
+      $(".third-question").show();
+
+    });
+
+});
+
+$(document).ready(function(){
+
+    $("#third-hide").click(function(){
+
+      $(".third-question").hide();
+      $(".fourth-question").show();
+
+    });
+
+});
+
+$(document).ready(function(){
+
+    $("#fourth-hide").click(function(){
+
+      $(".fourth-question").hide();
+      $(".fifth-question").show();
+
+    });
+
+});
+
